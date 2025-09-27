34d378717b517ce3559e1f7a2c645037efbac77f
arafa0001 <abdulrafayimran4@gmail.com>
2025-09-27 12:02:15 +0500

    importing react

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-import React from "react";
+// import React from "react";
+import  ReactDOM from "react-dom/client"
+
+const h1 = <h1>some text to append on window</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(h1)
